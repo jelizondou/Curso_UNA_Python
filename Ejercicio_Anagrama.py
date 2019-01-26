f45d46aeff5229e62b3e721d7d168b12b2816d4a
jelizondou <46851019+jelizondou@users.noreply.github.com>
2019-01-26 17:01:48 -0600

    Create Ejercicio_Anagrama.py

---
+++ Ejercicio_Anagrama.py
@@ -0,0 +1,43 @@
+palabra = input('Cual es la palabra:') 
+
+Cual es la palabra:jorge 
+
+  
+
+print(palabra) 
+
+jorge 
+
+  
+
+          if palabra [::] == palabra [::-1]: 
+
+     ...:     print('Es una anagrama') 
+
+     ...: else: 
+
+     ...:         print('No es una anagrama') 
+
+     ...: 
+
+No es una anagrama 
+
+  
+
+palabra = input('Cual es la palabra:') 
+
+Cual es la palabra:ana 
+
+  
+
+          if palabra [::] == palabra [::-1]: 
+
+     ...:     print('Es una anagrama') 
+
+     ...: else: 
+
+     ...:         print('No es una anagrama') 
+
+     ...: 
+
+Es una anagrama 
